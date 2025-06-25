60cfe7122c069b18bc3e1fc12fff8a661c006c02
Sachin <itssachinshivanand@gmail.com>
2025-06-25 07:17:26 +0530

    Add files via upload

---
+++ src/view-trip/[tripId]/components/InfoSection.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Button } from '@/components/ui/button'
+import { GetPlaceDetails } from '@/service/GlobalApi';
+import React, { useEffect } from 'react'
+import { FaShare } from "react-icons/fa6";
+
+
+function InfoSection({trip}) {
+  return (
+   <div className='max-w-xl mx-auto'>
+  <img className='h-[200px] sm:h-[280px] md:h-[340px] w-full object-cover rounded-xl' src="/placeholder.jpg" alt="" />
+  
+  <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center mt-5'>
+    <div className='flex flex-col gap-2'>
+      <h2 className='font-bold text-lg sm:text-xl md:text-2xl mt-2 sm:mt-0'>{trip?.userSelection?.location}</h2>
+      <div className='flex flex-wrap gap-3 sm:gap-5 mt-2'>
+        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-sm'>
+          📆 {trip?.userSelection?.nOofdays} Day
+        </h2>
+        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-sm'>
+          💰 {trip?.userSelection?.budget} Budget
+        </h2>
+        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-sm'>
+          💏 No. of traveler: {trip?.userSelection?.traveler}
+        </h2>
+      </div>
+    </div>
+    <Button className='mt-3 sm:mt-0 sm:ml-5'>
+      <FaShare />
+    </Button>
+  </div>
+</div>
+
+  )
+}
+
+export default InfoSection
